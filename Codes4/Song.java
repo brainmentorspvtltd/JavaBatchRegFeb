6a03c4afae1969a80255118702e5d02a90ba8313
amit4alljava <amit4alljava@gmail.com>
2017-03-07 14:29:20 +0530

    Code

---
+++ Codes4/Song.java
@@ -0,0 +1,13 @@
+// OOPS - Class and Object
+// What , Why and How 
+// OOAD - S O L I D
+// S - SRP (Single Responsiblity Principal)
+public class Song {
+	// Characters
+	int duration;  // Instance Member Variables of Class
+	String artistName;
+	String albumName;
+	String type;
+	String imagePath;
+
+}
---
+++ Codes4/TestSong.java
@@ -0,0 +1,20 @@
+
+public class TestSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Song boomboom = new Song();
+		boomboom.albumName="BOOM ";
+		boomboom.artistName="Mr X";
+		System.out.println(boomboom.albumName);
+		System.out.println(boomboom.artistName);
+		Song bangbang = new Song();
+		bangbang.albumName="Bang";
+		bangbang.artistName="Mr Y";
+		System.out.println("***************************");
+		System.out.println(bangbang.albumName);
+		System.out.println(bangbang.artistName);
+
+	}
+
+}
